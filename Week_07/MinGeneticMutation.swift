ae0c70e10211224ee6dfb510a69245b3c04b0ea3
杨冬武 <coderYDW@users.noreply.github.com>
2021-01-18 14:13:07 +0800

    基因变化：双向BFS

--- Week_07/MinGeneticMutation.swift
+++ Week_07/MinGeneticMutation.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 class MinGeneticMutation {
+    //单向BFS
     func minMutation(_ start: String, _ end: String, _ bank: [String]) -> Int {
         let bankSet = Set<String>(bank)
         guard bankSet.contains(end) else {
@@ -52,8 +53,55 @@ class MinGeneticMutation {
         return false
     }
     
+    //双向BFS
+    func minMutation10(_ start: String, _ end: String, _ bank: [String]) -> Int {
+        let bankSet = Set<String>(bank)
+        guard bankSet.contains(end) else {
+            return -1
+        }
+        var startSet: Set = [start], endSet: Set = [end], visited: Set = [start]
+        var res = 0
+        while !startSet.isEmpty && !endSet.isEmpty {
+            if startSet.count > endSet.count {
+                swap(&startSet, &endSet)
+            }
+            var nextSet = Set<String>()
+            for word in startSet {
+                if changeGenetic10(word, &nextSet, endSet, &visited, bankSet) {
+                    return res + 1
+                }
+            }
+            res += 1
+            startSet = nextSet
+        }
+        return -1
+    }
+    
+    func changeGenetic10(_ word: String, _ nextSet: inout Set<String>, _ endSet: Set<String>, _ visited: inout Set<String>, _ bankSet: Set<String>) -> Bool {
+        var word = Array(word)
+        for i in 0..<word.count {
+            let originalChar = word[i]
+            for c in "ACGT" {
+                if originalChar == c {
+                    continue
+                }
+                word[i] = c
+                let temp = String(word)
+                if endSet.contains(temp) {
+                    return true
+                }
+                if bankSet.contains(temp) && !visited.contains(temp) {
+                    nextSet.insert(temp)
+                    visited.insert(temp)
+                }
+                word[i] = originalChar
+            }
+        }
+        return false
+    }
+    
     func test() {
-        print(minMutation("AACCGGTT", "AACCGGTA", ["AACCGGTA"]))
+        print(minMutation10("AACCGGTT", "AACCGGTA", ["AACCGGTA"]))
     }
     
 }

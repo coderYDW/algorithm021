531a2bd3986b287e1de62b28e65cd879dd913707
杨冬武 <coderYDW@users.noreply.github.com>
2021-02-01 15:08:52 +0800

    Update LongestPalindromic.swift

--- Week_09/LongestPalindromic.swift
+++ Week_09/LongestPalindromic.swift
@@ -40,6 +40,63 @@ import Foundation
 
 class LongestPalindromic {
     
+    //动态规划(超时)
+    func longestPalindrome(_ s: String) -> String {
+        guard s.count > 1 else {
+            return s
+        }
+        let n = s.count
+        var dp = [[Bool]](repeating: [Bool](repeating: false, count: n), count: n)
+        var res = "", s = Array(s)
+        for i in (0..<n).reversed() {
+            for j in i..<n {
+                // 两边相等且长度小于等于3
+                // 两边相等且子串是回文串
+                dp[i][j] = s[i] == s[j] && (j - i < 3 || dp[i + 1][j - 1])
+                // 求最长子串
+                if dp[i][j] && j - i + 1 > res.count {
+                    res = String(s[i..<j + 1])
+                }
+            }
+        }
+        return res
+    }
+    
+    //动态规划
+    func longestPalindrome20(_ s: String) -> String {
+        guard s.count > 1 else {
+            return s
+        }
+        let n = s.count
+        var dp = [[Bool]](repeating: [Bool](repeating: false, count: n), count: n)
+        var maxLengh = 1, begin = 0, s = Array(s)
+        //中间赋值,空串
+        for i in 0..<n {
+            dp[i][i] = true
+        }
+        for j in 1..<n {
+            for i in 0..<j {
+                if s[i] != s[j] {
+                    //如果两边不等,不是回文串
+                    dp[i][j] = false
+                } else if j - i < 3 {
+                    //两边相等,且字符数小于等于3,是回文
+                    dp[i][j] = true
+                } else {
+                    //两边相等字符大于三,子串是就是,子串不是就不是
+                    dp[i][j] = dp[i + 1][j - 1]
+                }
+                //求所有是回文串的时候的最大长度
+                if dp[i][j] && j - i + 1 > maxLengh {
+                    maxLengh = j - i + 1
+                    begin = i
+                }
+                
+            }
+        }
+        return String(s[begin..<begin + maxLengh])
+    }
+    
     //中心扩展法
     func longestPalindrome10(_ s: String) -> String {
         guard s.count > 1 else {
@@ -66,5 +123,9 @@ class LongestPalindromic {
             r += 1
         }
         return r - l - 1
+    }
+    
+    func test() {
+        print(longestPalindrome("aaaaaa"))
     }
 }
